804f81c284a88f115863cd996640ca178ac36e73
ColeChan <chenwenjie27@gmail.com>
2016-05-05 15:25:02 +0800

    Fix api

--- pyos/__init__.py
+++ pyos/__init__.py
@@ -1,6 +1,6 @@
 __version__ = 0.1
 
-from pyos.pydocker import Docker
-from pyos.pyos.operation import SysOperation
-from pyos.pygit import GitClient
-from pyos.pygit.pygithub import GitHubClient
+from pydocker import Docker
+from pyos.operation import SysOperation
+from pygit import GitClient
+from pygit.pygithub import GitHubClient
